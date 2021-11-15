5196f7b464d2e62c059afc34ddef1fc7ee7750c9
FacuBar <efacubar@gmail.com>
2021-11-15 17:35:14 -0300

    replacing value for pointer to value in most interfaces

--- pkg/core/ports/users_repository.go
+++ pkg/core/ports/users_repository.go
@@ -7,9 +7,10 @@ import (
 
 type UsersRepository interface {
 	Get(int64) (*domain.User, rest_errors.RestErr)
+	Delete(int64) rest_errors.RestErr
+	GetByEmail(string) (*domain.User, rest_errors.RestErr)
+
 	Save(*domain.User) rest_errors.RestErr
 	Update(*domain.User) rest_errors.RestErr
 	UpdateAdmin(*domain.User) rest_errors.RestErr
-	Delete(int64) rest_errors.RestErr
-	GetByEmail(string) (*domain.User, rest_errors.RestErr)
 }
--- pkg/core/ports/users_service.go
+++ pkg/core/ports/users_service.go
@@ -2,12 +2,13 @@ package ports
 
 import (
 	"github.com/FacuBar/bookstore_users-api/pkg/core/domain"
+	"github.com/FacuBar/bookstore_utils-go/rest_errors"
 )
 
 type UsersService interface {
-	Register(domain.User) *domain.User
-	Login(domain.User) *domain.User
-	Update(domain.User) *domain.User
+	Register(*domain.User) rest_errors.RestErr
+	Update(*domain.User) rest_errors.RestErr
+	Login(string, string) (*domain.User, rest_errors.RestErr)
 
 	// Logout() *rest_errors.RestErr
 }
