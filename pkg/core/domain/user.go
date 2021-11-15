ff63fc45d3c136a07bffb291ebe8e954cb0c0c84
FacuBar <efacubar@gmail.com>
2021-11-15 17:32:47 -0300

    minor change to entity

--- pkg/core/domain/user.go
+++ pkg/core/domain/user.go
@@ -1,12 +1,13 @@
 package domain
 
 type User struct {
-	Id          int64  `json:"id"`
-	FirstName   string `json:"first_name"`
-	LastName    string `json:"last_name"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
-	DateCreated string `json:"date_created"`
-	Status      string `json:"status"`
-	Privileges  int    `json:"privileges"`
+	Id           int64  `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+	DateCreated  string `json:"date_created"`
+	LastModified string `json:"last_modified"`
+	Status       string `json:"status"`
+	Privileges   int    `json:"privileges"`
 }
